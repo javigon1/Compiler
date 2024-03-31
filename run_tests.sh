13782489a017cef8dffa3d177228dc5a9acacd46
javigon1 <javitogon98@hotmail.com>
2024-03-31 15:20:10 -0400

    shell tests prepared

---
+++ run_tests.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+UM_EXECUTABLE="um"
+TESTS_DIRECTORY="."
+
+# Get a list of all .um files in the current directory
+umFiles=$(ls $TESTS_DIRECTORY/*.um)
+
+# Loop over each .um file
+for testFile in $umFiles; do
+    # Remove the .um extension to get the test name
+    testName=$(echo $testFile | sed -E 's/(.*)\.um/\1/')
+
+    # Check if a corresponding .1 file exists
+    if [ -f "$TESTS_DIRECTORY/$testName.1" ]; then
+        # Run umdump on the .um file and redirect output to a .dump file
+        $UM_EXECUTABLE $testFile > $TESTS_DIRECTORY/$testName.dump
+    else
+        echo "$testName has no output!"
+    fi
+done
