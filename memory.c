449ee7c91878d569441f04371cd5c65da35aa663
javigon1 <javitogon98@hotmail.com>
2024-04-04 16:24:08 -0400

    .

--- memory.c
+++ memory.c
@@ -1,1 +1,7 @@
 #include "memory.h"
+
+typedef struct Memory {
+        Seq_T segments;
+        Seq_T unmappedIDs;
+        uint32_t program_counter;
+}
--- memory.h
+++ memory.h
@@ -1,5 +1,6 @@
 #ifndef MEMORY_H_INCLUDED
 #define MEMORY_H_INCLUDED
 
+#include <seq.h>
 
 #endif
